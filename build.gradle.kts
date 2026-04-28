cbdbeb839c2420ddb60e93231f0b8c6b65499449
Balazs.Dora <balazs.dora@apollosolutions.hu>
2026-04-28 09:58:47 +0200

    Remove testcontainers

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,9 +50,6 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation(platform("org.testcontainers:testcontainers-bom:1.20.4"))
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
