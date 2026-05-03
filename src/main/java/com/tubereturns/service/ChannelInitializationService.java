88ea0d33003cda807e8fbc29086c06c413fa94e3
Balazs.Dora <balazs.dora@apollosolutions.hu>
2026-05-03 15:04:16 +0200

    Only load channels from yaml in dev mode

--- src/main/java/com/tubereturns/service/ChannelInitializationService.java
+++ src/main/java/com/tubereturns/service/ChannelInitializationService.java
@@ -6,6 +6,7 @@ import com.tubereturns.repository.ChannelRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @RequiredArgsConstructor
 @Service
+@Profile("dev")
 @Transactional
 public class ChannelInitializationService {
 
